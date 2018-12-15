0056c279d9ddc88f27e7abb086ea35ebf31a578c
pahuja991 <rp824@njit.edu>
2018-12-14 22:45:43 -0500

    Laravel dusk setup and first register test

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.15 on 2018-12-07 23:30:43.
+ * Generated for Laravel 5.7.15 on 2018-12-14 16:28:53.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
